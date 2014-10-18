c1fba1ac5019853089a96eb2be4ff175ba4923b3
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2014-10-18 19:02:34 -0300

    Set a list of ToDos using Angular.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,10 @@
  */
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.todos = [
+      'Item 1',
+      'Item 2',
+      'Item 3',
+      'Item 4'
     ];
   });
